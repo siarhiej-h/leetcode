7b5ff3f57c21fd3cae19dce19047d0b6a6d4fb34
siarhiej-h <light.drow.777@gmail.com>
2022-01-18 00:47:27 +0100

    Updated solutions

---
+++ C#/TrappingRainWater42/Solution.cs
@@ -0,0 +1,73 @@
+﻿public class Solution
+{
+    private int CalculateForward(int[] heights, out int leftBoundary)
+    {
+        var segmentStart = Enumerable.Range(0, heights.Length - 2)
+            .Select((h, i) => (int?) i)
+            .TakeWhile(i => i.Value < heights.Length - 1)
+            .FirstOrDefault(i => heights[i.Value + 1] < heights[i.Value]);
+
+        if (segmentStart.HasValue)
+        {
+            (int segmentStart, int segmentLength, int trapHeight) SelectSegment(int segmentEnd)
+            {
+                var segment = (segmentStart.Value + 1, segmentEnd - segmentStart.Value - 1, heights[segmentStart.Value]);
+                segmentStart = segmentEnd;
+                return segment;
+            }
+
+            var segments = heights.Select((h, i) => i)
+                .Skip(segmentStart.Value + 1)
+                .Where(i => heights[i] >= heights[segmentStart.Value])
+                .Select(SelectSegment);
+            var sum = SumSegments(heights, segments);
+            leftBoundary = segmentStart.Value;
+            return sum;
+        }
+
+        leftBoundary = 0;
+        return 0;
+    }
+
+    private int CalculateBackward(int[] heights, int segmentStart)
+    {
+        int? segmentEnd = heights
+            .Select((h, i) => (int?) heights.Length - 1 - i)
+            .TakeWhile(i => i.Value > segmentStart + 1)
+            .FirstOrDefault(i => heights[i.Value - 1] < heights[i.Value]);
+
+        if (segmentEnd.HasValue)
+        {
+            (int segmentStart, int segmentLength, int trapHeight) SelectSegment(int segmentStart)
+            {
+                var segment = (segmentStart + 1, segmentEnd.Value - segmentStart - 1, heights[segmentEnd.Value]);
+                segmentEnd = segmentStart;
+                return segment;
+            }
+
+            var segments = Enumerable.Range(segmentStart, segmentEnd.Value - 1 - segmentStart)
+                .Reverse()
+                .Where(i => heights[i] >= heights[segmentEnd.Value])
+                .Select(SelectSegment);
+            return SumSegments(heights, segments);
+        }
+
+        return 0;
+    }
+
+    private int SumSegments(int[] heights, IEnumerable<(int segmentStart, int segmentLength, int trapHeight)> segments)
+    {
+        return segments.SelectMany(segment => heights
+            .Skip(segment.segmentStart)
+            .Take(segment.segmentLength)
+            .Select(height => segment.trapHeight - height))
+            .Sum();
+    }
+
+    public int Trap(int[] heights)
+    {
+        if (heights.Length < 3)
+            return 0;
+        return CalculateForward(heights, out var leftBoundary) + CalculateBackward(heights, leftBoundary);
+    }
+}
---
+++ C#/TrappingRainWater42/Solution2.cs
@@ -0,0 +1,51 @@
+﻿public class Solution2
+{
+    private static IEnumerable<(int segmentStart, int segmentEnd, int trapHeight)> FindSegments(int[] heights)
+    {
+        int? segmentStart = heights
+            .Select((h, i) => (int?)i)
+            .TakeWhile(i => i.Value < heights.Length - 1)
+            .FirstOrDefault(i => heights[i.Value + 1] < heights[i.Value]);
+
+        if (segmentStart.HasValue)
+        {
+            for (int i = segmentStart.Value + 1; i != heights.Length; i++)
+            {
+                if (heights[i] >= heights[segmentStart.Value])
+                {
+                    yield return (segmentStart.Value + 1, i, heights[segmentStart.Value]);
+                    segmentStart = i;
+                }
+            }
+        }
+
+        if (segmentStart.GetValueOrDefault(0) > heights.Length - 3)
+            yield break;
+
+        int? segmentEnd = heights
+            .Reverse()
+            .Select((h, i) => (int?)heights.Length - 1 - i)
+            .TakeWhile(i => i.Value > segmentStart.GetValueOrDefault(0) + 1)
+            .FirstOrDefault(i => heights[i.Value - 1] < heights[i.Value]);
+
+        if (segmentEnd.HasValue)
+        {
+            for (int i = segmentEnd.Value - 1; i >= segmentStart.GetValueOrDefault(0); i--)
+            {
+                if (heights[i] >= heights[segmentEnd.Value])
+                {
+                    yield return (i + 1, segmentEnd.Value, heights[segmentEnd.Value]);
+                    segmentEnd = i;
+                }
+            }
+        }
+    }
+
+    public int Trap(int[] height)
+    {
+        return FindSegments(height)
+            .Select(t => height[t.segmentStart..t.segmentEnd]
+            .Sum(height => t.trapHeight - height))
+            .Sum();
+    }
+}
