0baec4b105786df350f8b2f43c823ab3f62a7640
siarhiej-h <light.drow.777@gmail.com>
2022-01-23 18:50:46 +0100

    Added solution to https://leetcode.com/problems/number-of-ways-to-divide-a-long-corridor/

---
+++ C#/DivideLongCorridor2147/Program.cs
@@ -0,0 +1,5 @@
+﻿// https://leetcode.com/problems/number-of-ways-to-divide-a-long-corridor/
+
+var corridor = "SSPPSPS";
+var solution = new Solution();
+Console.WriteLine(solution.NumberOfWays(corridor));
---
+++ C#/DivideLongCorridor2147/Solution.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int NumberOfWays(string corridor)
+    {
+        int seats = corridor.Count(ch => ch == 'S');
+        if (seats % 2 != 0 || seats == 0)
+            return 0;
+
+        long basis = ((long)1e9) + 7;
+        return (int) GetMultipliers(corridor).Aggregate(1L, (a, b) => (a * b) % basis);
+    }
+
+    private static IEnumerable<int> GetMultipliers(string corridor)
+    {
+        var start = corridor.IndexOf('S', corridor.IndexOf('S') + 1) + 1;
+        while (start < corridor.Length)
+        {
+            int plants = Enumerable.Range(start, corridor.Length - start - 1)
+                .TakeWhile(i => corridor[i] == 'P')
+                .Count();
+
+            start = corridor.IndexOf('S', start + plants);
+            if (start == -1)
+                yield break;
+
+            yield return plants + 1;
+            start = corridor.IndexOf('S', start + 1) + 1;
+        }
+    }
+}
